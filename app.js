0b3726d3f3a76ff6354910a77aed4edbaea11d6a
Joshua Zhang <josh.zlxiang@gmail.com>
2024-02-10 16:17:03 -0500

    Added AutoComplete
    Added an autocomplete feature, replacing having the enter the coordinates of the origin and destination. Added a list of directions for the route too. Also polished the UI a bit.

--- app.js
+++ app.js
@@ -2,15 +2,67 @@ function loadMapScript() {
   const apiKey = document.getElementById('api-key').value;
   
   const script = document.createElement('script');
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&loading=async&callback=initMap`;
-  script.async = true;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&loading=async&libraries=places&callback=initGoogleMaps`;
 
   document.head.appendChild(script);
 }
 
-function initMap() {
-
+function initGoogleMaps() {
+  initMap();
+  initAutocomplete();
 }
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 8,
+      center: {lat: 43.69, lng: -79.37}
+    });
+}
+
+
+
+var originCoords;
+var destinationCoords;
+
+let originAutoComplete;
+let destinationAutocomplete;
+
+function initAutocomplete() {
+    originAutoComplete = new google.maps.places.Autocomplete(
+        document.getElementById("origin"), {
+          componentRestrictions: {"country": ["CA"]}
+        }
+    );
+    originAutoComplete.addListener('place_changed', onOriginPlaceChanged);
+
+    destinationAutocomplete = new google.maps.places.Autocomplete(
+        document.getElementById("destination")
+    );
+    destinationAutocomplete.addListener('place_changed', onDestinationPlaceChanged);
+}
+
+function onOriginPlaceChanged() {
+  var place = originAutoComplete.getPlace();
+  if (!place.geometry) {
+    console.log("OriginError");
+  } else {
+    var orgLat = place.geometry.location.lat();
+    var orgLng = place.geometry.location.lng();
+    originCoords = {lat: orgLat, lng: orgLng};
+  }
+}
+
+function onDestinationPlaceChanged() {
+  var place = destinationAutocomplete.getPlace();
+  if (!place.geometry) {
+    console.log("DestinationError");
+  } else {
+    var desLat = place.geometry.location.lat();
+    var desLng = place.geometry.location.lng();
+    destinationCoords = {lat: desLat, lng: desLng};
+  }
+}
+
   
 function createMap(origin, destination) {
     const map = new google.maps.Map(document.getElementById("map"), {
@@ -34,23 +86,34 @@ function createMap(origin, destination) {
       (response, status) => {
         if (status === "OK") {
           directionsRenderer.setDirections(response);
+          displayInstructions(response);
         } else {
           window.alert("Directions request failed due to " + status);
         }
       }
     );
 }
+
+function displayInstructions(directionsResult) {
+  var DirectionInstructions = document.getElementById("DirectionInstructions");
+  DirectionInstructions.innerHTML = '';
+  
+  var route  = directionsResult.routes[0];
+
+  for (var i = 0; i < route.legs.length; i++) {
+    var leg = route.legs[i];
+    
+    for (var j = 0; j < leg.steps.length; j++) {
+      var step = leg.steps[j];
+      var lat = step.start_location.lat();
+      var lng = step.start_location.lng();
+      DirectionInstructions.innerHTML += '<p>' + step.instructions + '</p>';
+      console.log(lat + "," + lng);
+    }
+  }
+}
   
 
 function updateRoute() {
-    const origin = {
-        lat: parseFloat(document.getElementById('origin-lat').value),
-        lng: parseFloat(document.getElementById('origin-lng').value)
-    };
-    const destination = {
-        lat: parseFloat(document.getElementById('destination-lat').value),
-        lng: parseFloat(document.getElementById('destination-lng').value)
-    };
-
-    createMap(origin, destination);
+    createMap(originCoords, destinationCoords);
 }
