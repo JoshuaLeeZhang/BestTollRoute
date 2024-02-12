ec60cd00be3985faa6f467727a1de40e451b92da
Joshua Zhang <josh.zlxiang@gmail.com>
2024-02-11 20:07:59 -0500

    407 Enter and Exit
    Created function to check where drive enters and exits the 407.

--- app.js
+++ app.js
@@ -19,27 +19,30 @@ function initMap() {
     });
 }
 
-var originCoords;
-var destinationCoords;
-
-let originAutoComplete;
-let destinationAutocomplete;
+let originCoords;
+let destinationCoords;
 
 function initAutocomplete() {
-    originAutoComplete = new google.maps.places.Autocomplete(
+    var originAutoComplete = new google.maps.places.Autocomplete(
         document.getElementById("origin"), {
           componentRestrictions: {"country": ["CA"]}
         }
     );
-    originAutoComplete.addListener('place_changed', onOriginPlaceChanged);
+    originAutoComplete.addListener('place_changed', function() {
+      onOriginPlaceChanged(originAutoComplete);
+    });
 
-    destinationAutocomplete = new google.maps.places.Autocomplete(
-        document.getElementById("destination")
+    var destinationAutocomplete = new google.maps.places.Autocomplete(
+        document.getElementById("destination"), {
+          componentRestrictions: {"country": ["CA"]}
+        }
     );
-    destinationAutocomplete.addListener('place_changed', onDestinationPlaceChanged);
+    destinationAutocomplete.addListener('place_changed', function() {
+      onDestinationPlaceChanged(destinationAutocomplete);
+    });
 }
 
-function onOriginPlaceChanged() {
+function onOriginPlaceChanged(originAutoComplete) {
   var place = originAutoComplete.getPlace();
   if (!place.geometry) {
     console.log("OriginError");
@@ -50,7 +53,7 @@ function onOriginPlaceChanged() {
   }
 }
 
-function onDestinationPlaceChanged() {
+function onDestinationPlaceChanged(destinationAutocomplete) {
   var place = destinationAutocomplete.getPlace();
   if (!place.geometry) {
     console.log("DestinationError");
@@ -61,7 +64,6 @@ function onDestinationPlaceChanged() {
   }
 }
 
-  
 function createMap(origin, destination) {
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 7,
@@ -85,6 +87,7 @@ function createMap(origin, destination) {
         if (status === "OK") {
           directionsRenderer.setDirections(response);
           displayInstructions(response);
+          travelTime(response);
         } else {
           window.alert("Directions request failed due to " + status);
         }
@@ -92,24 +95,61 @@ function createMap(origin, destination) {
     );
 }
 
+function travelTime(directionsResult) {
+  var travelTime = document.getElementById("travelTime");
+  travelTime.innerHTML = '';
+
+  var route = directionsResult.routes[0];
+
+  travelTime.innerHTML += '<p>' + directionsResult.routes[0].legs[0].duration.text + '</p>';
+}
+
 function displayInstructions(directionsResult) {
   var DirectionInstructions = document.getElementById("DirectionInstructions");
   DirectionInstructions.innerHTML = '';
-  
-  var route  = directionsResult.routes[0];
 
-  for (var i = 0; i < route.legs.length; i++) {
-    var leg = route.legs[i];
-    
-    for (var j = 0; j < leg.steps.length; j++) {
-      var step = leg.steps[j];
-      var lat = step.start_location.lat();
-      var lng = step.start_location.lng();
-      DirectionInstructions.innerHTML += '<p>' + step.instructions + '</p>';
-      console.log(lat + "," + lng);
-    }
+  for (var j = 0; j < directionsResult.routes[0].legs[0].steps.length; j++) {
+    var instruction = directionsResult.routes[0].legs[0].steps[j].instructions;
+    var lat = directionsResult.routes[0].legs[0].steps[j].start_location.lat();
+    var lng = directionsResult.routes[0].legs[0].steps[j].start_location.lng();
+    DirectionInstructions.innerHTML += '<p>' + instruction + " ";
+    DirectionInstructions.innerHTML += lat + "," + lng + '</p>';
+
+    // if (counter == 0) {
+    //   if (isToll(instruction)) {
+    //     counter++;
+    //   } else {
+    //     start407 = extractStreetName(instruction);
+    //   }
+    // }
+    // if (counter == 1) {
+    //   if (!isToll(instruction)) counter++;
+    //   end407 = extractStreetName(instruction);
+    // }
   }
 }
+
+function areCoordsClose(routeCoords, actualCoords, tolerance) {
+  var distance = sqrt((routeCoords.lat - actualCoords.lat)*(routeCoords.lat - actualCoords.lat) + (routeCoords.lng - actualCoords.lng)*(routeCoords.lng - actualCoords.lng));
+  return distance <= tolerance;
+}
+
+// function extractStreetName(string) {
+//   var parser = new DOMParser();
+//   var doc = parser.parseFromString(string, 'text/html');
+//   var boldElements = doc.querySelectorAll('b');
+//   return boldElements[1].textContent;
+// }
+
+function isToll(string) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(string, 'text/html');
+  return doc.body.textContent.includes('Toll road');
+}
+
+let start407;
+let end407;
+var counter = 0; // at 0 is before 407, at 1 is on 407, at 2 is after 407
   
 
 function updateRoute() {
